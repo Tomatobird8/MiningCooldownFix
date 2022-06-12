3dfbf8a071cf2b4e34b780e9f7d687a5ab77e635
Tomatobird8 <60841151+Tomatobird8@users.noreply.github.com>
2022-06-12 07:33:08 +0300

    Update and adjustments
    Updated to Minecraft 1.18.2
    Tweaked cooldown
    Removed forced block break resets (especially annoying on servers with high ping)

--- src/main/java/net/covenantturtle/miningcooldownfix/mixin/MiningCooldownFixMixin.java
+++ src/main/java/net/covenantturtle/miningcooldownfix/mixin/MiningCooldownFixMixin.java
@@ -1,14 +1,27 @@
 package net.covenantturtle.miningcooldownfix.mixin;
 
 import net.minecraft.client.network.ClientPlayerInteractionManager;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import net.minecraft.util.math.BlockPos;
 
 @Mixin(ClientPlayerInteractionManager.class)
 public abstract class MiningCooldownFixMixin {
+
+	@Shadow private BlockPos currentBreakingPos;
+	
 	@ModifyConstant(method = "updateBlockBreakingProgress", constant = @Constant(intValue = 5))
 	private int MiningCooldownFix(int value) {
-		return 0;
+		return 2;
+	}
+	
+	@Inject(method = "isCurrentlyBreaking", at = @At(value = "HEAD"), cancellable = true)
+	private void blChange(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(pos.equals(this.currentBreakingPos));
 	}
 }
